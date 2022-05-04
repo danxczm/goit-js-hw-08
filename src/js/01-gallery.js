938f94e5321196ca86532bd7816cacd9b961e8c5
danbohdan <81313913+danxczm@users.noreply.github.com>
2022-05-04 18:07:57 +0300

    second task is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,20 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
 
 function makeGallery(galleryArray) {
   return galleryArray
     .map(({ preview, original, description }) => {
-      return `<li>
-          <a class="gallery__item" href="${original}">
+      return `<a class="gallery__item" href="${original}">
           <img
           class="gallery__image"
           src="${preview}" 
           alt="${description}"
           style="display: block"
           />
-        </a>
-      </li>`;
+        </a>`;
     })
     .join('');
 }
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
